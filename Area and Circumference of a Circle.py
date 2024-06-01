c588769138fa2af69a8931b9aa37b96add75a72d
alidaquila917 <ivanchanwelding@gmail.com>
2024-05-31 21:00:55 -0700

    Update Area and Circumference of a Circle.py

--- Area and Circumference of a Circle.py
+++ Area and Circumference of a Circle.py
@@ -1,14 +1,20 @@
 import math
 
 # Take input for radius
+
 radius = float(input("Enter the radius of the circle: "))
+if radius>0:
+    
 
 # Calculate circumference
-circumference = 2 * math.pi * radius
+    circumference = 2 * math.pi * radius
 
 # Calculate area
-area = math.pi * (radius ** 2)
+    area = math.pi * (radius ** 2)
 
 # Print the calculated values
-print("Circumference:", circumference)
-print("Area:", area)
+    print("Circumference:", circumference)
+    print("Area:", area)
+else:
+    print("please enter a positive number")
+    
